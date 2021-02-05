b0f711d99d4203d2bd84d6189afed31d6171b9ab
Antonio <memphis2coder@gmail.com>
2021-02-04 22:13:06 -0500

    setup react router dom

---
+++ src/Pages/secondPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SecondPage() {
+    return (
+        <div>
+            <h2>Second Page</h2>
+        </div>
+    )
+};
+
+export default SecondPage;
--- src/components/NaviBar/Navbar.js
+++ src/components/NaviBar/Navbar.js
@@ -1,31 +1,44 @@
 import React from 'react';
+import {Link, Switch, Route} from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import './Navbar.css';
+
+import HomePage from '../../Pages/home';
+import SecondPage from '../../Pages/secondPage';
 
 function Navibar() {
     return (
         <div>
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="/">
-                    <span className="brandLogo">Transitions</span>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
-                        <Nav.Link href="#home">
-                            <span className='link'>
-                                LINK
-                            </span>    
-                        </Nav.Link>
-                        <Nav.Link href="#link">
-                            <span className='link'>
-                                LINK
-                            </span>
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+            <Navbar bg="none" expand="lg">
+                <Container>
+                    <Navbar.Brand as={Link} to='/'>
+                        <span className="brandLogo">Transitions</span>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link as={Link} to="/secondPage">
+                                <span className='link'>
+                                    Page2
+                                </span>    
+                            </Nav.Link>
+                            <Nav.Link href="#link">
+                                <span className='link'>
+                                    LINK
+                                </span>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
+            <div>
+                <Switch>
+                    <Route exact path='/' component={HomePage}/>
+                    <Route path='/secondPage' component={SecondPage} />
+                </Switch>
+            </div>
         </div>
     )
 };
